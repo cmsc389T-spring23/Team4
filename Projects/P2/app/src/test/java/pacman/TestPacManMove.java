50e5b06ad9793abf7b4902cc306742693c70701e
idys2 <irdnoun@gmail.com>
2023-02-21 17:32:56 -0500

    Revert "removed pacman move for consistency"
    This reverts commit 19a0e8e0291317d9864b286ee542f2e144638f07.

--- Projects/P2/app/src/test/java/pacman/TestPacManMove.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManMove.java
@@ -6,10 +6,6 @@ import java.awt.Color;
 public class TestPacManMove extends TestCase {
 
   public void testPacManMove() throws FileNotFoundException {
-    //Creating A Map
-    //Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
-    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
-    Ghost ghost = frame.addGhost(new Location(0, 0), "name", Color.red); //Creates a red ghost named "name" at location x,y
-    PacMan pacman = frame.addPacMan(new Location(0, 1)); //Creates PacMan at location x, y
+    return;
   }
 }
