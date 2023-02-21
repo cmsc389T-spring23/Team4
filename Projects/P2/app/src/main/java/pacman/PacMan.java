48197e6c30da617ffa8152af7722639e4ab6a5bd
kaneyumin <kaneatmc@gmail.com>
2023-02-21 11:53:51 -0500

    first completion of pacman_get-valid-moves

--- Projects/P2/app/src/main/java/pacman/PacMan.java
+++ Projects/P2/app/src/main/java/pacman/PacMan.java
@@ -4,23 +4,43 @@ import java.util.HashSet;
 import javax.swing.JComponent;
 
 public class PacMan {
-  String myName;
-  Location myLoc;
-  Map myMap;
-  Location shift;
+	String myName;
+	Location myLoc;
+	Map myMap;
+	Location shift;
+	
+	
+	public PacMan(String name, Location loc, Map map) {
+		this.myLoc = loc;
+		this.myName = name;
+		this.myMap = map;
+		
+		
+	}
 
-  public PacMan(String name, Location loc, Map map) {
-    this.myLoc = loc;
-    this.myName = name;
-    this.myMap = map;
-  }
+	public ArrayList<Location> get_valid_moves() {
 
-  public ArrayList<Location> get_valid_moves() {
-    return null;
-  }
+		int curr_x = myLoc.x;
+		int curr_y = myLoc.y;
+		// Check each location to see if a wall resides within. If not, add to ArrayList
+		ArrayList<Location> to_ret= new ArrayList<Location>();
+		Location potential_moves[] = {new Location(curr_x + 1, curr_y),
+									   new Location(curr_x - 1, curr_y),
+									   new Location(curr_x, curr_y + 1),
+									   new Location(curr_x, curr_y - 1)};
+
+		for(Location move: potential_moves){
+			HashSet<Map.Type> check = myMap.getLoc(move);
+			if(!check.contains(Map.Type.WALL)){
+				to_ret.add(move);
+			}
+		}
+		return to_ret;
+	}
 
   public boolean move() {
     return false;
+	
   }
 
   public boolean is_ghost_in_range() {
