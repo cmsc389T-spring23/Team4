ae7340472d57be4b974a706dfcf3953a4d323e27
idys2 <irdnoun@gmail.com>
2023-02-22 18:19:18 -0500

    added simple test for PacMan.move()

--- Projects/P2/app/src/test/java/pacman/TestPacManMove.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManMove.java
@@ -6,6 +6,12 @@ import java.awt.Color;
 public class TestPacManMove extends TestCase {
 
   public void testPacManMove() throws FileNotFoundException {
-    return;
+    Noframe frame = new NoFrame();
+    PacMan pacman = frame.addPacMan(new Location(0, 0));
+    assertTrue(pacman.move())
+    assertTrue(pacman.move())
+    assertTrue(pacman.move())
+    assertTrue(pacman.move())
+    assertTrue(pacman.move())
   }
 }
