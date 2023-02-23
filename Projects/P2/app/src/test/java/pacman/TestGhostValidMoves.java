a4a8749d0293df80ad510b9bff4476c049cd8661
kaneyumin <kaneatmc@gmail.com>
2023-02-23 10:13:09 -0500

    added test file

--- Projects/P2/app/src/test/java/pacman/TestGhostValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostValidMoves.java
@@ -6,6 +6,20 @@ import java.awt.Color;
 public class TestGhostValidMoves extends TestCase {
 
   public void testGhostValidMoves() throws FileNotFoundException {
-    return;
+	  
+	  NoFrame frame = new NoFrame();
+
+	  frame.addGhost(new Location(0,0));
+
+	  ArrayList<Location> check = new ArrayList<Location>();
+
+	  check = frame.ghost.get_valid_moves();
+
+	  //check if out of bounds move is valid. should be false
+	  assertFalse(check.contains(new Location(-1, 0)));
+	  //check if valid move is in list
+	  assertTrue(check.contains(new Location(1,0)));
+	  
+	  return;
   }
 }
