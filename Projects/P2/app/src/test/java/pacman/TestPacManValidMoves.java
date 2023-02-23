88fcd63e90aaf4d9334d59bad4a82e064282319a
kaneyumin <kaneatmc@gmail.com>
2023-02-23 10:12:36 -0500

    added test file

--- Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
@@ -6,6 +6,20 @@ import java.awt.Color;
 public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
-    return;
+
+	  NoFrame frame = new NoFrame();
+
+	  frame.addPacMan(new Location(0,0));
+
+	  ArrayList<Location> check = new ArrayList<Location>();
+
+	  check = frame.pacman.get_valid_moves();
+
+	  //check if out of bounds move is valid. should be false
+	  assertFalse(check.contains(new Location(-1, 0)));
+	  //check if valid move is in list
+	  assertTrue(check.contains(new Location(1,0)));
+	  
+	  return;
   }
 }
