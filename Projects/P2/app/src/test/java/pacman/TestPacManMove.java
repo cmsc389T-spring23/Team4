2079b323acc94c2f164a210426244808fc273108
idys2 <irdnoun@gmail.com>
2023-02-24 15:45:03 -0500

    fixed NoFrame

--- Projects/P2/app/src/test/java/pacman/TestPacManMove.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManMove.java
@@ -6,7 +6,7 @@ import java.awt.Color;
 public class TestPacManMove extends TestCase {
 
   public void testPacManMove() throws FileNotFoundException {
-    Noframe frame = new NoFrame();
+    NoFrame frame = new NoFrame();
     PacMan pacman = frame.addPacMan(new Location(0, 0));
     assertTrue(pacman.move())
     assertTrue(pacman.move())
