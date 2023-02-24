6a130be0f78ec005b5246b948777b468f9b9cbc4
idys2 <irdnoun@gmail.com>
2023-02-24 15:45:52 -0500

    fixed NoFrame

--- Projects/P2/app/src/test/java/pacman/TestGhostMove.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostMove.java
@@ -6,7 +6,7 @@ import java.awt.Color;
 public class TestGhostMove extends TestCase {
 
   public void testGhostMove() throws FileNotFoundException {
-    Noframe frame = new NoFrame();
+    NoFrame frame = new NoFrame();
     Ghost ghost = frame.addGhost(new Location(0, 0), "name", Color.red);
     assertTrue(ghost.move())
     assertTrue(ghost.move())
