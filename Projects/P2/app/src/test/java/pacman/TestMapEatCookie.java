2d6be1e0451e9c312004ee7ed0c6ad0ababcbdd3
Tom-Dai <technopupper@gmail.com>
2023-03-01 16:59:30 -0500

    fixed bugs

--- Projects/P2/app/src/test/java/pacman/TestMapEatCookie.java
+++ Projects/P2/app/src/test/java/pacman/TestMapEatCookie.java
@@ -7,14 +7,14 @@ public class TestMapEatCookie extends TestCase {
 
   public void testMapEatCookie() throws FileNotFoundException {
 	  NoFrame frame = new NoFrame();
-	  pacman= frame.addPacMan(new Location(1,1));
-	  map = frame.getMap();
+	  PacMan pacman= frame.addPacMan(new Location(1,1));
+	  Map map = frame.getMap();
 	  //should return a cookie component and increment cookies
-	  cookie =map.eatCookie("pacman");
-	  incremented = map.getCookies();
+	  JComponent cookie =map.eatCookie("pacman");
+	  int incremented = map.getCookies();
 	  //should return null and not increment
-	  noCookie= map.eatCookie("pacman");
-	  noincremented= map.getCookies();
+	  JComponent noCookie= map.eatCookie("pacman");
+	  int noincremented= map.getCookies();
 
 	  assertTrue(cookie!=null);
 	  assertTrue(noCookie==null);
