305be936b5595ae3f8e1318c540817f565bea8de
Tom-Dai <technopupper@gmail.com>
2023-03-01 16:37:46 -0500

    added asserts

--- Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
@@ -7,14 +7,17 @@ public class TestGhostAttack extends TestCase {
 
   public void testGhostAttack() throws FileNotFoundException {
 	Noframe frame = new Noframe();
+	Noframe frame2 = new Noframe();
 	pacman = frame.addPacMan(new Location(1,1));
+	pacman2= frame2.addPacMan(new Location(1,1));
 	ghost = frame.addGhost(new Location(1,2),"blinky",Color.red);
 	//a ghost is nearby pacman
 	near = ghost.attack();
-	farGhost = frame.addGhost(new Location(1,4), "sad",Color.red);
+	farGhost = frame2.addGhost(new Location(1,4), "sad",Color.red);
 	//a ghost is not nearby pacman
 	not_near= farGhost.attack();
-
-    	return near!=null && not_near==null;
+	assertTrue(near==true);
+	assertTrue(not_near==false);
+    	return
   }
 }
