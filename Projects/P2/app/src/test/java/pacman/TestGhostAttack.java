2f80540d4a898bc6cef6674689e2399f21abba43
Tom-Dai <technopupper@gmail.com>
2023-03-01 17:01:32 -0500

    fixed bugs

--- Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
@@ -6,16 +6,16 @@ import java.awt.Color;
 public class TestGhostAttack extends TestCase {
 
   public void testGhostAttack() throws FileNotFoundException {
-	Noframe frame = new Noframe();
-	Noframe frame2 = new Noframe();
-	pacman = frame.addPacMan(new Location(1,1));
-	pacman2= frame2.addPacMan(new Location(1,1));
-	ghost = frame.addGhost(new Location(1,2),"blinky",Color.red);
+	NoFrame frame = new NoFrame();
+	NoFrame frame2 = new NoFrame();
+	PacMan pacman = frame.addPacMan(new Location(1,1));
+	PacMan2= frame2.addPacMan(new Location(1,1));
+	Ghost ghost = frame.addGhost(new Location(1,2),"blinky",Color.red);
 	//a ghost is nearby pacman
-	near = ghost.attack();
-	farGhost = frame2.addGhost(new Location(1,4), "sad",Color.red);
+	boolean near = ghost.attack();
+	Ghost farGhost = frame2.addGhost(new Location(1,4), "sad",Color.red);
 	//a ghost is not nearby pacman
-	not_near= farGhost.attack();
+	boolean not_near= farGhost.attack();
 	assertTrue(near==true);
 	assertTrue(not_near==false);
     	return
