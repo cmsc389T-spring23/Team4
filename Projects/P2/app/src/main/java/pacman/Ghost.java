d532b1cf8578d039cd3f377bf2c1d8660c4b2c80
Wyatt Scott <112585814+wyscott123@users.noreply.github.com>
2023-03-29 14:03:38 -0400

    refixing ghost move

--- Projects/P2/app/src/main/java/pacman/Ghost.java
+++ Projects/P2/app/src/main/java/pacman/Ghost.java
@@ -39,14 +39,10 @@ public class Ghost {
 
   public boolean move() {
     ArrayList<Location> valid_moves = get_valid_moves();
-    if (valid_moves == null) {
-      return false;
-    }
-    int rand = (int) (Math.random() * valid_moves.size());
-    Location new_loc = valid_moves.get(rand);
-    myMap.move(myName, new_loc);
-    myLoc = new_loc;
-    return true;
+    // picks a random location from the list of valid moves
+    if (valid_moves != null && !valid_moves.isEmpty()) {
+      this.myLoc = valid_moves.get((int) (Math.random() * valid_moves.size()));
+      return myMap.move(this.myName, this.myLoc, Map.Type.GHOST);
   }
 
   public boolean is_pacman_in_range() {
